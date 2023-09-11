936a25a8524e4f23fe1b13f7d725c75bbeab397f
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-09-11 01:24:38 -0500

    cambio muy pequeño ruta corredor

--- src/Corredor/crearCorredor.js
+++ src/Corredor/crearCorredor.js
@@ -1,16 +1,14 @@
 const { Crearcorredor } = require("../db");
-
 const postCorredor = async (req, res) => {
   const { nombre, numero, imagen, descripcion } = req.body;
   try {
-    const newUser = await Crearcorredor.create({
+    const nuevocorredor = await Crearcorredor.create({
       nombre,
       numero,
       descripcion,
       imagen,
     });
-
-    return res.status(200).json(newUser);
+    return res.status(200).json(nuevocorredor);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
