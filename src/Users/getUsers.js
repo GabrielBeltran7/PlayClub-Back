8bf3e1a151b2902f88304fefdfc3bbe477ec6c4f
AngelSilva98 <angelceesd@gmail.com>
2023-09-14 21:59:52 -0300

    ruta usuario por id params

--- src/Users/getUsers.js
+++ src/Users/getUsers.js
@@ -1,7 +1,7 @@
 const { User } = require("../db");
 
 const getUsers = async (req, res) => {
-  const { username, email } = req.body;
+  const { username, email, id } = req.body;
   try {
     let users;
 
@@ -9,15 +9,22 @@ const getUsers = async (req, res) => {
       // Buscar usuarios por nombre de usuario
       users = await User.findAll({
         where: {
-          username: username
-        }
+          username: username,
+        },
       });
     } else if (email) {
       // Buscar usuarios por correo electrónico
       users = await User.findAll({
         where: {
-          email: email
-        }
+          email: email,
+        },
+      });
+    } else if (id) {
+      // Buscar usuarios por correo electrónico
+      users = await User.findAll({
+        where: {
+          id: id,
+        },
       });
     } else {
       // Si no se proporciona ni username ni email, devolver todos los usuarios
@@ -25,7 +32,9 @@ const getUsers = async (req, res) => {
     }
 
     if (!users || users.length === 0) {
-      return res.status(404).json({ error: "No se encontraron usuarios que coincidan con la búsqueda." });
+      return res.status(404).json({
+        error: "No se encontraron usuarios que coincidan con la búsqueda.",
+      });
     }
 
     return res.status(200).json(users);
@@ -34,6 +43,25 @@ const getUsers = async (req, res) => {
   }
 };
 
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    let user;
+    if (id) {
+      user = await User.findOne({
+        where: {
+          id: id,
+        },
+      });
+    }
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getUsers,
+  getUserById,
 };
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,11 +1,12 @@
-const {Router} = require("express")
-const { postUserHandler} = require("../Users/createusers")
-const {loginUsers} = require("../Users/loginusers")
-const {getUsers} = require ("../Users/getUsers")
+const { Router } = require("express");
+const { postUserHandler } = require("../Users/createusers");
+const { loginUsers } = require("../Users/loginusers");
+const { getUsers, getUserById } = require("../Users/getUsers");
 
 const usersRouter = Router();
-usersRouter.post("/", postUserHandler) 
-usersRouter.post("/login", loginUsers) 
-usersRouter.get("/", getUsers)
+usersRouter.post("/", postUserHandler);
+usersRouter.post("/login", loginUsers);
+usersRouter.get("/", getUsers);
+usersRouter.get("/:id", getUserById);
 
 module.exports = usersRouter;
