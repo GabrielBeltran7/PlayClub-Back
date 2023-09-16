8d81e0d63264ef24a23c31b6db854d66dddcde71
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-09-16 16:23:15 -0500

    actualizando url del back para que tome local o remoto

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require ("./src/app")
 const  {conn}= require ("./src/db")
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 server.listen(PORT,  ()=>{
     conn.sync({force: true});
     console.log(`Servidors escuchando en el Puerto ${PORT}`);
