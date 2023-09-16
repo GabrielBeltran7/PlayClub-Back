b2422348dacb47a56ffda45990a3a8eb27fe8ce9
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-09-16 17:17:27 -0500

    creacion de ruta get, para que funcione el servidor

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const  {conn}= require ("./src/db")
 const PORT = process.env.PORT || 3001
 server.listen(PORT,  ()=>{
     conn.sync({force: true});
+    server.get("/", (req, res) =>{
+        res.send("servidor de win123")
+    })
     console.log(`Servidors escuchando en el Puerto ${PORT}`);
 })
