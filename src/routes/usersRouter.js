f2abf6da6b719e981137178bdec8aa8755185416
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-09-19 16:50:22 -0500

    crear get de mis apuestas

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -4,13 +4,15 @@ const { loginUsers } = require("../Users/loginusers");
 const { getUsers, getUserById } = require("../Users/getUsers");
 const {getUserByUsername} = require ("../Users/getUsersUsernameLogin")
 const {getCarreraActiva} = require ("../Users/getCarreraactiva")
-
+const {getLinkcamaras} = require("../Users/getLinkcamaras")
 const usersRouter = Router();
 usersRouter.post("/", postUserHandler);
 usersRouter.post("/login", loginUsers);
+usersRouter.get("/linkcamaras", getLinkcamaras) 
 usersRouter.get("/", getUsers);
 usersRouter.get("/:id", getUserById);
 usersRouter.get("/getUserByUsername/:username", getUserByUsername);
 usersRouter.get("/carreraactiva/:nombrecarrera", getCarreraActiva) 
+
 
 module.exports = usersRouter;
