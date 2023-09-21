63912cf56eaa3f09dfa08871760f2b984573d4f4
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-09-20 22:57:43 -0500

    actualizar

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { conn } = require("./src/db");
 const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
-  conn.sync({ force: false });
+  conn.sync({ force: true });
 
   console.log(`Servidors escuchando en el Puerto ${PORT}`);
 });
--- src/db.js
+++ src/db.js
@@ -23,7 +23,7 @@ const CrearlinkcamarasModel= require("./models/CrearLinkcamara")
 // );
 //aaaa
 // // CONEXION PARA SUBIR A RENDER  ********************
-// const { EXTERNAL_HOST } = process.env;
+ const { EXTERNAL_HOST } = process.env;
 const sequelize = new Sequelize(
   EXTERNAL_HOST,
 
@@ -56,8 +56,6 @@ User.hasMany(Recargarpuntos, {
   onUpdate: "CASCADE",
 });
 Recargarpuntos.belongsTo(User);
-// //Relacion de Uno a Muchos(Una carrera  tiene muchos corredores)
-
 
 Crearcarrera.hasMany(Crearcorredor, {
   onDelete: "CASCADE",
