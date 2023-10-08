df962549b75e730941a78b848a7f150a8d3a6a74
GabrielBeltran7 <gabrielbeltranmedina@gmail.com>
2023-10-08 00:11:43 -0500

    actualizar servidor

--- src/models/RecargarPuntos.js
+++ src/models/RecargarPuntos.js
@@ -19,7 +19,7 @@ module.exports = (sequelize) => {
         allowNull: true,
       },
       
-      usernameSubadmin: {
+      usernameAdmin: {
         type: DataTypes.STRING,
         allowNull: true,
       }
